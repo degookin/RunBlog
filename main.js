3ac0b5635ffc4bfa1f639ab949be7c0a12c0b37e
Dennis Gookin <degookin>
2019-04-25 13:31:30 -0400

    lots of updates

---
+++ main.js
@@ -0,0 +1,60 @@
+var marathons = [];
+
+
+marathons.push({
+    title: "Chicago Marathon",
+    year: 2012,
+    medalPic: "https://images.unsplash.com/photo-1508730328641-47c1616341b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80",
+    racePic: "",
+    time: "4:08:08",
+    pace: "",
+    location: "Chicago, IL",
+    map: ""
+},
+{
+    title: "Disney World Marathon",
+    year: 2015,
+    medalPic: "https://images.unsplash.com/photo-1508730328641-47c1616341b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80",
+    racePic: "",
+    time: "4:15:00",
+    pace: "",
+    location: "Lake Buena Vista, FL",
+    map: ""
+});
+
+
+// function buildString(marathon){
+//     var result = "I ran the ";
+//     result += marathon.title + " in ";
+//     result += marathon.year;
+//     result += " with a time of " + marathon.time;
+//     return result;
+// }
+
+// marathons.forEach(function(marathon){
+//     console.log(buildString(marathon));
+// });
+
+
+
+//TESTING1
+// const keys = Object.keys(marathons);
+const values = Object.values(marathons);
+// const entries = Object.entries(marathons);
+// console.log(keys);
+// console.log(values);
+// console.log(entries);
+
+values.forEach(marathon => {
+    console.log("I ran the " + marathon.title + " in " + 
+    marathon.year +  " and my time was " + marathon.time);
+});
+
+
+
+//TESTING2
+
+
+
+
+
