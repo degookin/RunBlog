543090bb76482d649e42fa2dbaa1887f7fe37be3
degookin <degookin@gmail.com>
2022-10-13 15:56:53 -0400

    Update marathons.js

--- marathons.js
+++ marathons.js
@@ -4,71 +4,57 @@ marathons.push({
     title: "Chicago Marathon",
     year: 2012,
     medalPic: "assets/ChicagoM2012.jpg",
-    racePic: "",
     time: "4:08:08",
     pace: "9:28",
-    location: "Chicago, IL",
-    map: ""
+    location: "Chicago, IL"
 },
 {
     title: "Chicago Marathon",
     year: 2013,
     medalPic: "assets/ChicagoM2013.jpg",
-    racePic: "",
     time: "4:08:29",
     pace: "9:29",
-    location: "Chicago, IL",
-    map: ""
+    location: "Chicago, IL"
 },
 {
     title: "Chicago Marathon",
     year: 2014,
     medalPic: "assets/ChicagoM2014.jpg",
-    racePic: "",
     time: "4:15:05",
     pace: "9:44",
-    location: "Chicago, IL",
-    map: ""
+    location: "Chicago, IL"
 },
 {
     title: "Disney World Marathon",
     year: 2015,
     medalPic: "assets/DisneyM2015.jpg",
-    racePic: "",
     time: "4:21:17",
     pace: "9:58",
-    location: "Lake Buena Vista, FL",
-    map: ""
+    location: "Lake Buena Vista, FL"
 },
 {
     title: "Derby Festival Marathon",
     year: 2016,
     medalPic: "assets/DerbyM2016.jpg",
-    racePic: "",
     time: "4:25:07",
     pace: "10:07",
-    location: "Louisville, KY",
-    map: ""
+    location: "Louisville, KY"
 },
 {
     title: "Chicago Marathon",
     year: 2016,
     medalPic: "assets/ChicagoM2016.jpg",
-    racePic: "",
     time: "3:55:49",
     pace: "8:59",
-    location: "Chicago, IL",
-    map: ""
+    location: "Chicago, IL"
 },
 {
     title: "Chicago Marathon",
     year: 2019,
     medalPic: "",
-    racePic: "",
     time: "4:18:53",
     pace: "9:53",
-    location: "Chicago, IL",
-    map: ""
+    location: "Chicago, IL"
 });
 
 const mValues = Object.values(marathons);
